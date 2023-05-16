6662717f7c70f2e836251e44e28c81741e71e44a
Ceasor7 <josegee0707@gmail.com>
2023-05-16 19:08:55 +0300

    fifth commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,11 @@ import React from 'react'
 const Document = () => {
   return (
     <Html lang='en'>
-      <Head>         
-          <meta name="robots" content="index, follow" />     
+      <Head>
+          <meta name="description" content="CALI is a non-profit organization that focuses on promoting arts management in the country. It provides research, training, education, and resources to empower individuals and organizations working in the arts sector." />
+          <meta name="robots" content="index, follow" />
+          <meta name='tags' content='a premier leadership development organization focused on empowering creative professionals to lead with vision, purpose, and impact.' />
+       
       </Head>
         <body>
             <Main />
