9ccfff69ff6cd7afd535f5435e9597ae7a2a629b
Ceasor7 <josegee0707@gmail.com>
2023-05-16 19:04:06 +0300

    Fourth commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,8 @@ import React from 'react'
 const Document = () => {
   return (
     <Html lang='en'>
-      <Head>
-          <meta name="description" content="An company profile of Collosal Company. Its company that serve for Application Development (Mobile, Web and Desktop), UI UX Design and Maintenance" />
-          <meta name="robots" content="index, follow" />
-          <meta name='tags' content='web,app,ui,maintenance,company,collosal,profile' />
-       
+      <Head>         
+          <meta name="robots" content="index, follow" />     
       </Head>
         <body>
             <Main />
